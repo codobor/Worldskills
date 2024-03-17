3b349597d00a33e405d3c436fc7030318b5496f5
codobor <codobor@mail.ru>
2024-03-17 16:52:00 +0300

    1

---
+++ Проект Visual Studio/hospital/Clients.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hospital
+{
+    /// <summary>
+    /// Логика взаимодействия для Patients.xaml
+    /// </summary>
+    public partial class Clients : Window
+    {
+        public Clients()
+        {
+            InitializeComponent();
+            try
+            {
+                string query = "SELECT * FROM clients";
+                MySqlConnection conn = new MySqlConnection(connectionString);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable table = new DataTable();
+                conn.Open();
+                adapter.Fill(table);
+                dataGridClients.ItemsSource = table.DefaultView;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        string connectionString = "server=localhost;user=root;database=hospital;port=3306;password=1234";
+
+
+    }
+}
